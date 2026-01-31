14622d22c393e9de137fef00ac2d28b97f9e8687
Hash Anu <hashmak.jsn@gmail.com>
2026-01-31 16:19:56 +0530

    fixing

--- src/pager.c
+++ src/pager.c
@@ -3832,4 +3832,24 @@ void sqlite3pager_refdump(Pager *pPager){
 }
 #endif
 
+/*
+** Invoke the given busy handler.
+**
+** This routine is called when an operation failed with a lock.
+** If this routine returns non-zero, the lock is retried.  If it
+** returns 0, the operation aborts with an SQLITE_BUSY error.
+*/
+int sqlite3InvokeBusyHandler(BusyHandler *p){
+  int rc;
+  if( p==0 || p->xFunc==0 || p->nBusy<0 ) return 0;
+  rc = p->xFunc(p->pArg, p->nBusy);
+  if( rc==0 ){
+    p->nBusy = -1;
+  }else{
+    p->nBusy++;
+  }
+  return rc;
+}
+
+
 #endif /* SQLITE_OMIT_DISKIO */
